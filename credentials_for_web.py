bdf4536049f1140e3915dabed90241191cc2a1f5
Fred Bahr <flbahr@mbari.org>
2024-11-07 07:58:18 -0800

    credential file form but with usernames and pw removed

---
+++ credentials_for_web.py
@@ -0,0 +1,7 @@
+EMAIL_ACCOUNT = 'mail_with_data_from_vehicle'
+USERNAME = 'username_of_above_account'
+PW = 'Password_to_account'
+IMAP_SVR = 'your_mail_server' # incoming mail server
+IMAP_PORT = 993
+SMTP_SVR = 'your_mail_server' # outgoing mail server xxxx.xxxx.edu or something
+SMTP_PORT = 587
